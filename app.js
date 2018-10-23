d5fcf7dfd814386bb0e7c7a146ebe881f3efff56
Lyndsay Miles <lymiles@gmail.com>
2018-10-23 20:18:10 +0000

    environment variables

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes= require("./routes/campgrounds"),
     indexRoutes=     require("./routes/index");
 
-console.log(process.env.DATABASEURL);
-mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true });
-// mongoose.connect("mongodb://LyndsayM:am02mm04@ds163382.mlab.com:63382/yelpcamp_lmiles");
+var url = process.env.DATABASEURL || "mongodb://localhost:27017/yelp_camp";
+mongoose.connect(url, { useNewUrlParser: true });
+
 app.use(bodyParser.urlencoded({extended: true}));  
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
